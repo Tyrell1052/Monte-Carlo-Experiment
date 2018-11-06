636c9bcc14abf0cf0949e78d68be06a6156b6347
Tyrell <Tyrell1052@gmail.com>
2018-11-06 17:31:49 -0500

    saving

--- src/MonteCarlo.java
+++ src/MonteCarlo.java
@@ -4,9 +4,15 @@ public class MonteCarlo{
 
     public static void main(String[] args){
 
-        int[] oddNumbers = {1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,36};
-        String red = String.valueOf(oddNumbers);
-        String red = "Red";
+
+
+        //GameSimulate(100);
+        //GameSimulate(1000);
+        //GameSimulate(1000000);
+
+        //int[] oddNumbers = {1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,36};
+        //String red = String.valueOf(oddNumbers);
+        //String red = "Red";
 
 
         //int[] oddNumbers = {1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,36};
@@ -14,5 +20,44 @@ public class MonteCarlo{
 
         System.out.println(oddNumbers);
 
+    }
+    public void GameSimulate (int spins){
+        /*
+        int blackTotal = 0;
+        int blackCurrentStreak = 0;
+        int blackMaxStreak = 0;
+        int redTotal = 0;
+        int redCurrentStreak = 0;
+        int redMaxStreak = 0;
+        int greenTotal = 0;
+        int greenCurrentStreak = 0;
+        int greenMaxStreak = 0;
+
+        for (int i=1; i <= spins; i++){
+
+            int randomNumber = Generate Random number from 0 - whatever;
+            if (if black){
+                blackTotal += 1;
+                blackCurrentStreak += 1;
+                if (redCurrentStreak > redMaxStreak){
+                    redMaxStreak = redCurrentStreak;
+                    redCurrentStreak = 0;
+                }
+                else {
+                    reset current streak;
+                }
+                if (greenCurrentStreak < greenMaxStreak){
+                    Do same thing as red did
+                }
+            }
+            if (if red) {
+                Do same as black
+            }
+            if (if green) {
+                Do same as black
+            }
+        Print results;
+        }
+         */
     }
 }
