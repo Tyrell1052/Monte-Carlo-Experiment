b52923ce7e4b6cb6b80aa1bac9cc87357251df6c
Tyrell1052 <tyrell1052@gmail.com>
2018-11-06 10:38:07 -0500

    cant figure out anything.. just saving

---
+++ src/MonteCarlo.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class MonteCarlo{
+
+    public static void main(String[] args){
+
+        int[] oddNumbers = {1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,36};
+        String red = String.valueOf(oddNumbers);
+        String red = "Red";
+
+
+        //int[] oddNumbers = {1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,36};
+        //int[] red = oddNumbers;
+
+        System.out.println(oddNumbers);
+
+    }
+}
--- src/main.java
+++
@@ -1,6 +0,0 @@
-class MonteCarlo{
-    public static void main(String[] args){
-        System.out.println("Hello World");
-
-    }
-}
