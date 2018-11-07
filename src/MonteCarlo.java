950f82d7b592eaf1461ff4978b2660926d434fc2
Tyrell1052 <tyrell1052@gmail.com>
2018-11-07 18:29:07 -0500

    not sure if while loop logic is correct

--- src/MonteCarlo.java
+++ src/MonteCarlo.java
@@ -8,7 +8,6 @@ public class MonteCarlo {
         // setting variables
 
         // variable for the random number
-        int randomNumber = 0;
 
         // total black, red and green
         int totalBlack = 0;
@@ -23,10 +22,12 @@ public class MonteCarlo {
 
         // using random class to generate the random numbers
         Random randomNum = new Random();
+        int randomNumber;
         int count = 0; // setting sound to 0
-        while (count <= 100) { // while logic for conditional statement
+        while (count <= 10) { // while logic for conditional statement
+            //int randomNumber;
             randomNumber = randomNum.nextInt(37);// this will choose random number between 0 and 36
-
+            //if statement to determine is the number is odd or even and add
             if (randomNumber == 0) {// if statement for landing on Green
                 totalGreen++;
                 count++;
@@ -36,24 +37,26 @@ public class MonteCarlo {
             if (randomNumber % 2 == 0) {
                 if (randomNumber >= 1 && randomNumber <= 10 || randomNumber >= 19 && randomNumber <= 28){
                     totalBlack++;
+                    count++;
                 }
                 else
                 {
                     totalRed++;
+                    count++;
                 }
             }else{
                 if (randomNumber >= 1 && randomNumber <= 10 || randomNumber >= 19 && randomNumber <= 28) {
                     totalRed++;
+                    count++;
                 }
                 else
                 {
                     totalBlack++;
+                    count++;
                 }
             }
-
-            //System.out.println(randomNumber);
-            //count++;// adds 1 to the count at the end of the while loop
-            if (randomNumber % 2 == 0);{
+            // if statement for the max streak of odds and evens
+            if (randomNumber % 2 == 0){
 
                 currentEvenCount++;
 
@@ -82,64 +85,11 @@ public class MonteCarlo {
 
 
         }
+        System.out.println("The total number of times a red number appeared:" + totalRed);
+        System.out.println("The total number of times a black number appeared:" + totalBlack);
+        System.out.println("The total number of times a green number appeared.:" + totalGreen);
+        System.out.println("The longest streak of even numbers:" + maxEvenStreak);
+        System.out.println("The longest streak of odd numbers:" + maxOddStreak);
     }
 }
-
-
-
-
- /*
-
-        for (int i=1; i <= spins; i++){
-
-            int randomNumber = Generate Random number from 0 - whatever;
-            if (if black){
-                blackTotal += 1;
-                blackCurrentStreak += 1;
-                if (redCurrentStreak > redMaxStreak){
-                    redMaxStreak = redCurrentStreak;
-                    redCurrentStreak = 0;
-                }
-                else {
-                    reset current streak;
-                }
-                if (greenCurrentStreak < greenMaxStreak){
-                    Do same thing as red did
-
-               -------------------------
-                    String[] rouletteNumbers = new String[37];
-
-        for(int i = 0; i < rouletteNumbers.length; i++) {
-            if (i % 2 == 0) {
-                rouletteNumbers[i] = "Red";
-            } else {
-
-                rouletteNumbers[i] = "Black";
-            }
-
-            System.out.println(rouletteNumbers[i]);
-
---------------------------------------------------------------------
-String[] rouletteNumbers = new String[37]; // creating empty string with a total of 37 indexes
-
-        for(int i = 0; i < rouletteNumbers.length; i++) {//this sets i to 0 - the for loop will,
-            //count from i to the length of rouletteNumbers
-            if (i % 2 == 0) { // this will determine if the number is odd or even by using modulus division
-                rouletteNumbers[i] = "Black";// if even set the the sting "Black"
-                totalBlack ++ ; // add 1 to the totalBlack variable
-                //System.out.println(totalBlack);
-
-            } else {
-
-                rouletteNumbers[i] = "Red";// if even set the the sting "Red"
-                totalRed ++;// add 1 to the totalRed variable
-                //System.out.println(totalRed);
-            }
-
-            //System.out.println(rouletteNumbers[i]);
-
-
-        }
-
-         */
 
